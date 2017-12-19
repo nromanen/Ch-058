56a15bcc1929660db0db8e30c635c1917d002fe2
shrralis <yaroslavzhyravov@gmail.com>
2017-12-20 00:04:46 +0200

    - New code [NOT WORKING].

--- src/main/java/com/shrralis/ssdemo1/configuration/SecurityConfig.java
+++ src/main/java/com/shrralis/ssdemo1/configuration/SecurityConfig.java
@@ -12,17 +12,27 @@
 
 package com.shrralis.ssdemo1.configuration;
 
-import com.shrralis.ssdemo1.entity.User;
-import com.shrralis.ssdemo1.security.*;
+import com.shrralis.ssdemo1.security.RESTAuthenticationEntryPoint;
+import com.shrralis.ssdemo1.security.RESTAuthenticationFailureHandler;
+import com.shrralis.ssdemo1.security.RESTAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * @author shrralis (https://t.me/Shrralis)
@@ -33,6 +43,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${security.signing-key}")
+    private String signingKey;
+    @Value("${security.encoding-strength}")
+    private Integer encodingStrength;
+    @Value("${security.security-realm}")
+    private String securityRealm;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     private RESTAuthenticationEntryPoint authenticationEntryPoint;
     private RESTAuthenticationFailureHandler authenticationFailureHandler;
     private RESTAuthenticationSuccessHandler authenticationSuccessHandler;
@@ -54,30 +80,62 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic()
+                .realmName(securityRealm)
+                .and()
+                .csrf().disable();
+        /*http.csrf().disable().
                 authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.POST, "/login", "/signUp").permitAll()
                 .antMatchers("/admin/**").hasRole(User.Type.ADMIN.name())
                 .anyRequest().authenticated()
                 .and()
-                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                *//*.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)*//*
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
                 .and()
                 .formLogin().successHandler(authenticationSuccessHandler)
                 .and()
-                .formLogin().failureHandler(authenticationFailureHandler);
+                .formLogin().failureHandler(authenticationFailureHandler);*/
     }
 
-    // TODO: remove this after testing
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder(encodingStrength));
+        // TODO: remove this after testing
         // Create a default account
-        auth.inMemoryAuthentication()
+        /*auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("password")
-                .roles("ADMIN");
+                .roles("ADMIN");*/
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+
+        converter.setSigningKey(signingKey);
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    @Primary
+    //Making this primary to avoid any accidental duplication with another token service instance of the same name
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
     }
 }
