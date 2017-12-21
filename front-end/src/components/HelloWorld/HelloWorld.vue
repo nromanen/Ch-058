49ec40d72a39fd06e2d23b08fae14c0d4d29e504
shrralis <yaroslavzhyravov@gmail.com>
2017-12-21 21:33:27 +0200

    - Added fully working Vue.js project.

---
+++ front-end/src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template src="./index.html"></template>
+
+<script src="./main.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./style.css"></style>
