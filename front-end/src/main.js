b5f86c1def19d55d27f5806dcbd7b969c569f98d
shrralis <yaroslavzhyravov@gmail.com>
2017-12-22 17:46:02 +0200

    Front-end:  - Added login page.
    Back-end:
    [none]

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,9 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import Vuelidate from 'vuelidate'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import App from './App'
 import router from './router/index'
 
+Vue.use(Vuelidate)
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld/HelloWorld'
+import HelloWorld from '@/components/pages/HelloWorld/HelloWorld'
+import SignInPage from '@/components/pages/SignInPage/SignInPage'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'SignInPage',
+      component: SignInPage
     }
   ]
 })
