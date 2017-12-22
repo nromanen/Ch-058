d72556f9113a1854bc9427e0bb3ae81b6004df75
shrralis <shrralis@gmail.com>
2017-12-22 14:38:58 +0200

    - Fixing Vue.js.

--- front-end/build/webpack.base.conf.js
+++ front-end/build/webpack.base.conf.js
@@ -8,9 +8,7 @@ function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
-const createLintingRule = () =;
->
-({
+const createLintingRule = () => ({
   test: /\.(js|vue)$/,
   loader: 'eslint-loader',
   enforce: 'pre',
@@ -42,93 +40,53 @@ module.exports = {
   },
   module: {
     rules: [
-      ...(config.dev.useEslint ? [createLintingRule()] : []),;
-{
-  /\.vue$/,
-    loader;
-:
-  'vue-loader',
-    options;
-:
-  vueLoaderConfig
-}
-,
-{
-  /\.js$/,
-    loader;
-:
-  'babel-loader',
-    include;
-:
-  [resolve('src'), resolve('test')]
-}
-,
-{
-  /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-    loader;
-:
-  'url-loader',
-    options;
-:
-  {
-    10000,
-      name;
-  :
-    utils.assetsPath('img/[name].[hash:7].[ext]')
-  }
-}
-,
-{
-  /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-    loader;
-:
-  'url-loader',
-    options;
-:
-  {
-    10000,
-      name;
-  :
-    utils.assetsPath('media/[name].[hash:7].[ext]')
-  }
-}
-,
-{
-  /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-    loader;
-:
-  'url-loader',
-    options;
-:
-  {
-    10000,
-      name;
-  :
-    utils.assetsPath('fonts/[name].[hash:7].[ext]')
-  }
-}
-]
-},
-{
-  // prevent webpack from injecting useless setImmediate polyfill because Vue
-  // source contains it (although only uses it if it's native).
-  false,
+      ...(config.dev.useEslint ? [createLintingRule()] : []),
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
     // prevent webpack from injecting mocks to Node native modules
     // that does not make sense for the client
-    dgram;
-:
-  'empty',
-    fs;
-:
-  'empty',
-    net;
-:
-  'empty',
-    tls;
-:
-  'empty',
-    child_process;
-:
-  'empty'
-}
-}
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+};
