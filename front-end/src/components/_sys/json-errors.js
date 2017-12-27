38c8412f70d2c2fc0ee8911adf2b3d5e93178f77
shrralis <yaroslavzhyravov@gmail.com>
2017-12-27 13:19:33 +0200

    # Shrralis commit changelog #34:
        [GLOBAL]:
         `null`
    
        [Front-end]:
         - Added Home button to auth forms.
    
        [Back-end]:
         `null`
    
        [Database]:
         `null`

--- front-end/src/components/_sys/json-errors.js
+++ front-end/src/components/_sys/json-errors.js
@@ -1,11 +1,11 @@
-export const NO_ERROR = 0;
-export const UNEXPECTED = 1;
-export const ACCESS_DENIED = 2;
-export const IMAGE_ALREADY_EXISTS = 3;
-export const MAP_MARKER_ALREADY_EXISTS = 4;
-export const USER_ALREADY_EXISTS = 5;
-export const MISSING_PARAMETER = 6;
-export const BAD_PARAMETER_FORMAT = 7;
+export const NO_ERROR = 0
+export const UNEXPECTED = 1
+export const ACCESS_DENIED = 2
+export const IMAGE_ALREADY_EXISTS = 3
+export const MAP_MARKER_ALREADY_EXISTS = 4
+export const USER_ALREADY_EXISTS = 5
+export const MISSING_PARAMETER = 6
+export const BAD_PARAMETER_FORMAT = 7
 
 export function getErrorMessage(error) {
   switch (error.errno) {
