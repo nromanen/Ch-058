fa081aae7c4b8ac8efa11142d193f0beec09327c
shrralis <yaroslavzhyravov@gmail.com>
2018-01-04 18:08:50 +0200

    # Shrralis commit changelog #37:
    ##    [GLOBAL]:
         - Added some new security logic.
    
    ##    [Front-end]:
         - Fixed bug with logout.
         - Added latest error constants.
         - New `md-chip` styles.
    
    ##    [Back-end]:
         - Fixed bug with logout.
         - Added new error constants.
         - New checking on failed authentication attempts.
         - New checking on "ddos" with creating recovery tokens.
    
    ##    [Database]:
    	 - New column in table `users`, that is `failed_auth_count`.

--- front-end/src/components/_validator/LoginOrEmailValidator.js
+++ front-end/src/components/_validator/LoginOrEmailValidator.js
@@ -1,4 +1,4 @@
-import {LOGIN_PATTERN} from "./LoginValidator";
+import {LOGIN_PATTERN} from './LoginValidator'
 
 export const PATTERN = /(^$|^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$)/
 
