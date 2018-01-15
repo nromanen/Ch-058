4e1c5d80f81f2a7f0a2f34b55697fa7603e40f1f
yurakiziuk <yurakizyuk@gmail.com>
2018-01-15 17:10:00 +0200

    Test for controller

--- src/test/java/com/shrralis/ssdemo1/controller/MapRestControllerTest.java
+++ src/test/java/com/shrralis/ssdemo1/controller/MapRestControllerTest.java
@@ -1,29 +1,31 @@
 package com.shrralis.ssdemo1.controller;
 
 import com.shrralis.ssdemo1.configuration.AppConfig;
-import com.shrralis.ssdemo1.configuration.DatabaseConfig;
 import com.shrralis.ssdemo1.dto.MarkerDTO;
-import com.shrralis.ssdemo1.service.interfaces.IIssueService;
-import com.shrralis.ssdemo1.service.interfaces.IMapMarkersService;
+import com.shrralis.ssdemo1.service.MapMarkersServiceImpl;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -40,41 +42,31 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 @WebAppConfiguration
 public class MapRestControllerTest {
 
-	@Autowired
-	private WebApplicationContext wac;
-	private MockMvc mockMvc;
+	@Mock
+	private MapMarkersServiceImpl service;
 
-	@Autowired
 	@InjectMocks
 	private MapRestController controller;
 
+	private MockMvc mockMvc;
+
 	@Before
 	public void setUp() throws Exception {
+
 		MockitoAnnotations.initMocks(this);
-		this.mockMvc = standaloneSetup(this.wac).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
 
 	@Test
 	public void test() throws Exception {
 
-		this.mockMvc.perform(get("/map").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk());
-	}
+		List<MarkerDTO> markers = new ArrayList<>();
+		markers.add(new MarkerDTO( 1.123456, 6.54321, 1));
+		markers.add(new MarkerDTO( 9.909999, 8.88888, 1));
 
-//	@Test
-//	public void testLoadAllMarkers() throws Exception {
-//		mockMvc.perform(MockMvcRequestBuilders.get("/map/").accept(MediaType.APPLICATION_JSON))
-//				.andExpect(jsonPath("$", hasSize(7)));
-//		List<MarkerDTO> markers = Arrays.asList(
-//				new MarkerDTO( 1.123456, 6.54321, 1),
-//				new MarkerDTO( 9.909999, 8.88888, 1));
-//
-//		when(markerService.loadAllMarkers()).thenReturn(markers);
-//
-//		mockMvc.perform(get("/map/"))
-//
-//
-//		verify(markerService, times(1)).loadAllMarkers();
-//		verifyNoMoreInteractions(markerService);
-//	}
+		when(service.loadAllMarkers()).thenReturn(markers);
+		mockMvc.perform(get("/map").accept(MediaType.APPLICATION_JSON));
+		verify(service, times(1)).loadAllMarkers();
+		verifyNoMoreInteractions(service);
+	}
 }
